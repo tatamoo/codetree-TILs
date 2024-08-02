515b07de1ab7731aa0b024fbf5ffcae2a9526807
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-02 23:44:53 +0900

    Title: 숫자의 계절은, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240802/숫자의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+month = int(input())
+
+if month>=3 and month<=5:
+    print("Spring") 
+elif month>=6 and month<=8:
+    print("Summer")
+elif month>=9 and month<=11:
+    print("Fall")
+else:
+    print("Winter")
