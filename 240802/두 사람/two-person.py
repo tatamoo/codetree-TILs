a33cc2767ba3a79611254c31c26cbc248343ec29
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-02 23:54:05 +0900

    Title: 두 사람, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240802/두 사람/two-person.py
@@ -0,0 +1,9 @@
+a_age,a_sex = input().split()
+a_age = int(a_age)
+b_age,b_sex = input().split()
+b_age = int(b_age)
+
+if a_age>=19 and a_sex == "M" or b_age>=19 and b_sex == "M":
+    print(1)
+else:
+    print(0)
