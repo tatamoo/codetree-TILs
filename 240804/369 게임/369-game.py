844a7ffdaff3e9c973f64413f510b55ab85d1fa2
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-04 17:42:31 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240804/369 게임/369-game.py
+++ 240804/369 게임/369-game.py
@@ -7,7 +7,7 @@ for i in range(1,n+1):
         if i<10:
             print(i,end=' ')
         else:
-            temp = string(i)
+            temp = string(i).split('')
             for i in range(0,2):
                 if temp[i]=='3'or temp[i]=='6' or temp[i]=='9':
                     print(0,end=' ')
