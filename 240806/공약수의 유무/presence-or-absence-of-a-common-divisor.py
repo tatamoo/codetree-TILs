429c6a822b24d1a8e15511b0639784b9969fe923
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-06 22:15:26 +0900

    Title: 공약수의 유무, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240806/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -2,7 +2,7 @@ a,b = input().split()
 a,b = int(a),int(b)
 result = False
 for i in range(a,b+1):
-    if 1920%1==0 and 2880%i==0:
+    if 1920%i==0 and 2880%i==0:
         result = True
 
 if result == True:
