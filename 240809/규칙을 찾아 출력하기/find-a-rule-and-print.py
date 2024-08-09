e00379300f1d57a45ec6b59434f2c03236fa900b
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-09 22:38:08 +0900

    Title: 규칙을 찾아 출력하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240809/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -6,10 +6,8 @@ print()
 
 for i in range(n-1):
     for j in range(n):
-        if j==n-1:
+        if j==n-1 or j<=i:
             print("*",end=' ')
         else:
-            for _ in range(i):
-                print("*",end=' ')
-            for _ in range(n-i-1):
-                print(" ",end=' ')
+            print(" ",end=' ')
+    print()
