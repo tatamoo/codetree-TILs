e11ec9794d18b6247cc955a9cf29ce606dd8c2b4
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-08-15 22:20:12 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240815/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
+++ 240815/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -5,12 +5,13 @@ even = 0
 
 for elem in arr:
     if elem==0:
-        for i in range(cnt):
-            if arr[i]%2==0:
-                sum += arr[i]
-                even += 1
         break
     else:
         cnt += 1
+
+for i in range(cnt):
+    if arr[i]%2==0:
+        sum += arr[i]
+        even += 1
 
 print(even,sum)
