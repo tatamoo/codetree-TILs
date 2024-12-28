2167fda9ed98decce6e6a8d33d38d152745aafcd
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-12-28 21:40:34 +0900

    Title: 십의 자리 숫자, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241228/십의 자리 숫자/number-of-tens-digit.py
+++ 241228/십의 자리 숫자/number-of-tens-digit.py
@@ -1,9 +1,11 @@
 cnt = [0] * 10
 arr = list(map(int, input().split()))
+i = 0
 
-for i in range(len(arr)-1):
+while arr[i] != 0:
     if arr[i]>=10:
         cnt[arr[i]//10] += 1
+    i += 1
     
 for j in range(1,10):
     print(f"{j} - {cnt[j]}")
