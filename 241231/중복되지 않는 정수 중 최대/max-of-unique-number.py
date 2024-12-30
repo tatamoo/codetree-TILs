3e9cd8044d836f87f8571176a1570ca0f3b751fd
tatamoo <159868595+tatamoo@users.noreply.github.com>
2024-12-31 01:58:35 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 241231/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,16 +1,9 @@
 n = int(input())
 a = list(map(int, input().split()))
-c = [0] * n
-max = 0
 
-for i in range(n):
-    c[a[i]-1] += 1
+unique_nums = [num for num in a if a.count(num) == 1]
 
-for j in range(n):
-    if c[j] == 1:
-        max = j+1
-
-if max == 0:
-    print(-1)
+if unique_nums:
+    print(max(unique_nums))
 else:
-    print(max)
+    print(-1)
